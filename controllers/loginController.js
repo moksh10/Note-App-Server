a18d33e328c2dca29f90a8614c2fa855309935e6
MOKSH <mokshteng@gmail.com>
2023-06-29 11:28:39 +0530

    fix

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -54,6 +54,7 @@ exports.login = async (req,res) => {
    }
    catch(error)
    {
+       console.log(error)
        res.status(500).json({message:"Server Error"})
    }
 
--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,42 +1,27 @@
-const jwt = require("jsonwebtoken")
-const User = require("../models/User")
-const path = require('path')
-require('dotenv').config({path:path.join(__dirname+"/../.env")})
-exports.auth = async (req,res,next) => {
-    try
-    {
-        if(!req.cookies)
-        {
-            return res.status(403).json({message:"Please Login"})
-            
-        }
-        const token = req.cookies.token
-        jwt.verify(token,process.env.JWT_PASSWORD,(err,decoded)=>{
-            if(err)
-            {
-                return res.status(403).json({message:"Invalid Authorization"})
-                  
-            }
-            req.id = decoded.id
-            if(req.path==="/auth")
-            {
-                
-            res.json({loggedIn:true})
-            return
-
-            }
-            next()
-            return
-        })
-        
-    
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname + "/../.env") });
+exports.auth = async (req, res, next) => {
+  try {
+    if (!req.cookies) {
+      return res.status(403).json({ message: "Please Login" });
     }
-    catch(error)
-    {
-        res.status(500).json({message:"Server Error"})
-        
-
-    }
-   
-
-}
+    const token = req.cookies.token;
+    jwt.verify(token, process.env.JWT_PASSWORD, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid Authorization" });
+      }
+      req.id = decoded.id;
+      if (req.path === "/auth") {
+        res.json({ loggedIn: true });
+        return;
+      }
+      next();
+      return;
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
